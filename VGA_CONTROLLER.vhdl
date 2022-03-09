9df874284231b1f67cc64ca992d03b881feaf2b5
maj77 <38226349+maj77@users.noreply.github.com>
2022-03-09 14:13:27 +0100

    Update VGA_CONTROLLER.vhdl

--- VGA_CONTROLLER.vhdl
+++ VGA_CONTROLLER.vhdl
@@ -21,23 +21,23 @@ entity VGA_CONTROLLER is
 	
 	generic(
 		------- HORIZONTAL PUlSE WIDTH VALUES --------
-		H_PIXELS			: INTEGER := 640;		-- visible area
+		H_PIXELS			: INTEGER := 640;	-- visible area
 		H_FRONT_PORCH		: INTEGER := 16;
 		H_SYNC_PULSE		: INTEGER := 96;	
 		H_BACK_PORCH		: INTEGER := 48;	 
 		H_SYNC_POLARITY		: STD_LOGIC := '0';		-- polarity of horizontal sync pulse
 		
 		------- VERTICAL PUlSE WIDTH VALUES --------
-		V_PIXELS			: INTEGER := 480;		-- visible area
+		V_PIXELS			: INTEGER := 480;	-- visible area
 		V_FRONT_PORCH		: INTEGER := 10;
 		V_SYNC_PULSE		: INTEGER := 2;
 		V_BACK_PORCH		: INTEGER := 33;
-		V_SYNC_POLARITY		: STD_LOGIC := '0'	    -- polarity of vertical sync pulse
+		V_SYNC_POLARITY		: STD_LOGIC := '0'	    	-- polarity of vertical sync pulse
 		); 	
 	
 	port(
 		------- PORT DECLARATIONS --------
-		CLK			: in STD_LOGIC;												-- increments over every pixel
+		CLK			: in STD_LOGIC;											-- increments over every pixel
 		RESET		: in STD_LOGIC;	 											-- asynchronous reset active low  
 		CE			: in STD_LOGIC;
 		H_SYNC		: out STD_LOGIC;											-- horizontal sync signal
@@ -61,12 +61,12 @@ begin
 		--variable H_COUNT : INTEGER range 0 to H_PERIOD - 1 := 0; -- counts columns
 		--variable V_COUNT : INTEGER range 0 to V_PERIOD - 1 := 0; -- counts rows
 	begin
-		if RESET = '1' then    					--reset asserted
+		if RESET = '1' then    			    --reset asserted
 			H_COUNT <= (others => '0');         --reset horizontal counter
 			V_COUNT <= (others => '0');         --reset vertical counter
-			H_SYNC <= not H_SYNC_POLARITY;     	--deassert horizontal sync
+			H_SYNC <= not H_SYNC_POLARITY;      --deassert horizontal sync
 			V_SYNC <= not V_SYNC_POLARITY;      --deassert vertical sync
-			DISP_EN <= '0';          		  	--disable display
+			DISP_EN <= '0';          	    --disable display
 			COL <= (others => '0');             --reset column pixel coordinate
 			ROW <= (others => '0');             --reset row pixel coordinate
 			
@@ -89,7 +89,7 @@ begin
 				if H_COUNT < H_PIXELS + H_FRONT_PORCH or H_COUNT >= H_PIXELS + H_FRONT_PORCH + H_SYNC_PULSE then  -- check if horizontal counter signal is out of horizontal sync range
 					H_SYNC <= not H_SYNC_POLARITY;	  		-- disable horizontal sync pulse
 				else
-					H_SYNC <= H_SYNC_POLARITY;				-- enable horizontal sync pulse
+					H_SYNC <= H_SYNC_POLARITY;			-- enable horizontal sync pulse
 				end if;	-- horizontal sync signal
 				
 				
@@ -97,13 +97,13 @@ begin
 				if V_COUNT < V_PIXELS + V_FRONT_PORCH or V_COUNT >= V_PIXELS + V_FRONT_PORCH + V_SYNC_PULSE then   -- check if vertical counter signal is out of vertical sync range
 					V_SYNC <= not V_SYNC_POLARITY;			-- disable vertical sync pulse
 				else
-					V_SYNC <= V_SYNC_POLARITY;		  		-- enable vertical sync pulse
+					V_SYNC <= V_SYNC_POLARITY;		  	-- enable vertical sync pulse
 				end if; -- vertical sync signal		   	
 				
 				
 				---- set pixel coordinates ----
 				if H_COUNT < H_PIXELS then	-- horizontal time
-					COL <= H_COUNT;		  	-- set horizontal pixel coordinates
+					COL <= H_COUNT;		-- set horizontal pixel coordinates
 				end if;
 				if V_COUNT < V_PIXELS then
 					ROW <= V_COUNT;
